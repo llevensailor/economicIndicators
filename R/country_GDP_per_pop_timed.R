19e7cb715abdd158d7d151bbca628b9ac68854f1
Adajli <ajli@smith.edu>
2026-04-08 11:58:22 -0400

    Modification to the function

--- R/country_GDP_per_pop_timed.R
+++ R/country_GDP_per_pop_timed.R
@@ -3,6 +3,6 @@ country_GDP_per_pop_timed <- function(data,country){
     dplyr::filter(Country == country) |>
     select(Country, Year, Population, `Gross Domestic Product (GDP)`) |>
     mutate(GDP_per_pop = `Gross Domestic Product (GDP)`/Population)
-  lm(Year~Population, data = data)
+  print(lm(Year~Population, data = data))
   ggplot(data = data, mapping = aes(x = Year, y = Population)) + geom_line()
 }
