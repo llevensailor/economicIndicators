91982250bfb7c92615a261d083fb9455d1cfad6e
Adajli <ajli@smith.edu>
2026-04-08 12:07:26 -0400

    Rough idea of population comparison function

--- R/population_comparison.R
+++ R/population_comparison.R
@@ -1,4 +1,7 @@
 library(ggplot2)
 population_comparison <- function(data,country1,country2,year){
-
+  data <- data |>
+    dplyr::filter(Country == country1, Country == country2, Year == year) |>
+    select(Country, Year, Population)
+  print(data$Population)
 }
