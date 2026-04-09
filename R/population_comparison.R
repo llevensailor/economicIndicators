a7bb13f2d3f6adf895fe16cf9426fe71647cbda2
Adajli <ajli@smith.edu>
2026-04-09 16:19:25 -0400

    Attempt to revise function

--- R/population_comparison.R
+++ R/population_comparison.R
@@ -1,7 +1,12 @@
 library(ggplot2)
 population_comparison <- function(data,country1,country2,year){
   data <- data |>
-    dplyr::filter(Country == country1, Country == country2, Year == year) |>
+    dplyr::filter(Country == country1, Year == year) |>
     select(Country, Year, Population)
+  data2 <- data |>
+    dplyr::filter(Country == country2, Year == year) |>
+    select(Country, Year, Population)
+
   print(data$Population)
+  print(data2$Population)
 }
