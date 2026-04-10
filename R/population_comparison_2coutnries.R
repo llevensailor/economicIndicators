ee7e4b93b234d25e871bbe2dfc307ba09cefd6a3
Adajli <ajli@smith.edu>
2026-04-10 10:45:58 -0400

    Function update

--- R/population_comparison_2coutnries.R
+++ R/population_comparison_2coutnries.R
@@ -1,15 +1,16 @@
-library(ggplot2)
-population_comparison <- function(data,country1,country2,year){
+library(dplyr)
+population_comparison_2countries <- function(data,country1,country2,year){
   if(year<1970 || year>2021){
     stop("Please type in a year that is within the range of 1970 and 2021")
   }
+  data2 <- data
   data <- data |>
     dplyr::filter(Country == country1, Year == year) |>
     select(Country, Year, Population)
-  data2 <- data |>
+  data2 <- data2 |>
     dplyr::filter(Country == country2, Year == year) |>
     select(Country, Year, Population)
 
-  print(data$Population)
-  print(data2$Population)
+  print(paste("The population for", country1, "in",year,"was", data$Population))
+  print(paste("The population for", country2, "in",year,"was", data2$Population))
 }
