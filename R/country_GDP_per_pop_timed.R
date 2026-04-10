e3c0ed814f3f917643d4e21769c43dc57cfc7b07
Adajli <ajli@smith.edu>
2026-04-10 11:34:03 -0400

    Function rename

--- R/country_GDP_per_pop_timed.R
+++ R/country_GDP_per_pop_timed.R
@@ -1,11 +1,13 @@
+library(fpp3)
 country_GDP_per_pop_timed <- function(data,country){
   data <- data |>
     dplyr::filter(Country == country) |>
     select(Country, Year, Population, `Gross Domestic Product (GDP)`) |>
-    mutate(GDP_per_pop = `Gross Domestic Product (GDP)`/Population)
-  print(lm(Year~Population, data = data))
-  ggplot(data = data, mapping = aes(x = Year, y = Population)) +
-    geom_point() +
-    geom_smooth(method = "lm") +
-    labs(title = paste("Plot of the line for", country))
+    mutate(GDP_per_pop = `Gross Domestic Product (GDP)`/Population,Year_dif = Year-1970)
+
+  df <- ts(data$Population, frequency = 1, start = 1970)
+  plot(df, main = paste(country, "'s Population over Time"),ylab = "Population")
+
+  print(lm(Population~Year_dif, data = data))
+
 }
