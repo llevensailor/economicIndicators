d59cf3e20fec5c554756cc8cae1561e7bb252c21
Adajli <ajli@smith.edu>
2026-04-09 20:36:57 -0400

    Added an error in case the user inserts the wrong year.

--- R/gdp_per_pop1yr.R
+++ R/gdp_per_pop1yr.R
@@ -1,5 +1,8 @@
 library(dplyr)
 gdp_per_pop1yr <- function(data,country,year){
+  if(year<1970 || year>2021){
+    stop("Please type in a year that is within the range of 1970 and 2021")
+  }
   data <- data |>
     dplyr::filter(Country == country, Year == year) |>
     select(Country, Year, Population, `Gross Domestic Product (GDP)`) |>
--- R/population_comparison.R
+++ R/population_comparison.R
@@ -1,5 +1,8 @@
 library(ggplot2)
 population_comparison <- function(data,country1,country2,year){
+  if(year<1970 || year>2021){
+    stop("Please type in a year that is within the range of 1970 and 2021")
+  }
   data <- data |>
     dplyr::filter(Country == country1, Year == year) |>
     select(Country, Year, Population)
