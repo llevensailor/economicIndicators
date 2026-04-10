aca07836304bbf1502e1c24181dedc3670d22f15
Adajli <ajli@smith.edu>
2026-04-10 11:45:10 -0400

    Updates to the country gdp per population function

--- R/country_GDP_per_pop_timed.R
+++ R/country_GDP_per_pop_timed.R
@@ -5,8 +5,8 @@ country_GDP_per_pop_timed <- function(data,country){
     select(Country, Year, Population, `Gross Domestic Product (GDP)`) |>
     mutate(GDP_per_pop = `Gross Domestic Product (GDP)`/Population,Year_dif = Year-1970)
 
-  df <- ts(data$Population, frequency = 1, start = 1970)
-  plot(df, main = paste(country, "'s Population over Time"),ylab = "Population")
+  df <- ts(data$GDP_per_pop, frequency = 1, start = 1970)
+  plot(df, main = paste(country, "'s GDP per Population over Time"),ylab = "Population")
 
   print(lm(Population~Year_dif, data = data))
 
