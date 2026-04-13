301051a2acb1d6b935c388d6945d205af6849cf3
Adajli <ajli@smith.edu>
2026-04-13 11:07:22 -0400

    Function update

--- R/ag_hunt_nature.R
+++ R/ag_hunt_nature.R
@@ -1,4 +1,14 @@
-
+#'Agricultural, hunting, forestry, fishing indicator
+#'@description Determines  the indicator for Agricultural, hunting, forestry, fishing
+#'
+#'@name ag_hunt_nature
+#'@details Takes in the data and selects a country set a certain year to find that indicator
+#'in that country in a certain year.
+#'@param data the economicIndicators data set
+#'@param country the country to input
+#'@param year the year set for the country
+#'@import dplyr
+#'@examples ag_hunt_nature(economicIndicators_data,'Spain', 1970)
 ag_hunt_nature<- function(data,country,year) {
   data <- data |>
     dplyr::filter(Country == country, Year == year) |>
