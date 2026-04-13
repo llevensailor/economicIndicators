03cd63f2c78323930f3166b603fed511c08fb448
Adajli <ajli@smith.edu>
2026-04-13 11:28:09 -0400

    Updated test file.

--- tests/testthat/test-gdp_per_pop1yr.R
+++ tests/testthat/test-gdp_per_pop1yr.R
@@ -1,8 +1,11 @@
 test_that("gdp per pop 1 year works", {
-
   expect_equal(gdp_per_pop1yr(economicIndicators_data,'Spain', 1970), 1213.051073)
+
+  error_test <-gdp_per_pop1yr(economicIndicators_data,'Spain', 1965)
   # expect error of the year is out of range
-  expect_error(gdp_per_pop1yr(economicIndicators_data,'Spain', 1965, "Please type in a year that is within the range of 1970 and 2021"))
+  expect_error(error_test,"Please type in a year that is within the range of 1970 and 2021")
+
+
 
 }
 )
