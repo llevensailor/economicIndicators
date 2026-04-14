8e1871c3e3bd4187baef4328c7ce90085d75526f
Adajli <ajli@smith.edu>
2026-04-13 21:24:10 -0400

    Creating the dataset version in R

---
+++ R/economicIndicators_data.R
@@ -0,0 +1,7 @@
+#'Economic indicators dataset
+#'@description Determines a country's GDP per population over time.
+#'
+#'@name economicIndicators_data
+#'@docType data
+#'@details Data that shows different economic indicators such as population, GDP, and the ama exchange rate.
+"economicIndicators_data"
