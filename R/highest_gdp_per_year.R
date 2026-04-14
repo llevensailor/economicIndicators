98d84cd07fc87292b219c0bbaa3f07926e9ea5df
Adajli <ajli@smith.edu>
2026-04-14 11:29:21 -0400

    Update function details

--- R/highest_gdp_per_year.R
+++ R/highest_gdp_per_year.R
@@ -2,7 +2,7 @@
 #'@description Determines the country with the highest GDP based on the given year
 #'
 #'@name highest_gdp_per_year
-#'@details
+#'@details Filters a year and then returns the top one using head(1)
 #'@param data the economicIndicators data set
 #'@param year the year set for the country
 #'@import dplyr
