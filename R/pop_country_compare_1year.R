86e36b068eed42b0754a7d80a7de8226f423b00a
Adajli <ajli@smith.edu>
2026-04-22 11:55:21 -0400

    Modifications to let the function be expanded on and have package handle imports

--- R/pop_country_compare_1year.R
+++ R/pop_country_compare_1year.R
@@ -10,9 +10,9 @@
 #'@param year the year set for comparison of the countries
 #'@importFrom dplyr filter select mutate
 #'@importFrom rlang .data
+#'@import ggplot2 dplyr
 #'@export
 #'@examples pop_country_compare_1year(economicIndicators_data,'Spain', 'Italy', 1970)
-library(dplyr)
 pop_country_compare_1year <- function(data,country1,country2,year){
   if(year<1970 || year>2021){
     stop("Please type in a year that is within the range of 1970 and 2021")
@@ -27,6 +27,8 @@ pop_country_compare_1year <- function(data,country1,country2,year){
   data2 <- data2 |>
     dplyr::filter(Country == country2, Year == year) |>
     dplyr::select(Country, Year, Population)
-
-  return(paste("The population for", country1, "in",year,"was", data$Population, "while the population for", country2, "in",year,"was", data2$Population))
+  data_comp <- rbind(data,data2)
+  ggplot2::ggplot(data_comp, aes(x= Country, y = Population, fill = Country)) +
+    geom_col() +
+    ggplot2::labs(title = paste("The population comparison of two countries:", country1 , "and", country2),caption = paste("The population for", country1, "in",year,"was", data$Population, "while the population for", country2, "in",year,"was", data2$Population))
 }
